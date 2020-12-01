d2f552913e140e60fc4d941209bf1da4bd9d3b10
naayem <vincentnaayem@gmail.com>
2020-12-02 00:36:31 +0100

    added the start tutorial

--- src/telegram_interface/telegram_interface.py
+++ src/telegram_interface/telegram_interface.py
@@ -20,6 +20,16 @@ class TelegramInterface:
         chat_id = msg['chat']['id']
 
         if msg['text'] == "/capture_image":
-            print("we send the image")
-            cv2.imwrite("data/img.jpg", self.cam.read()[1])
-            self.bot.sendPhoto(chat_id, open("data/img.jpg", "rb"))
+            self.capture_image(chat_id)
+
+        if msg['text'] == "/start":
+            self.start_tutorial(chat_id)
+
+    def capture_image(self, chat_id):
+        print("we send the image")
+        cv2.imwrite("data/img.jpg", self.cam.read()[1])
+        self.bot.sendPhoto(chat_id, open("data/img.jpg", "rb"))
+
+    def start_tutorial(self, chat_id):
+        tutorial = "Welcome to lakeye, try to take a picture with /capture_image :)"
+        self.bot.sendMessage(chat_id, tutorial)
