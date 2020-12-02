50ea87cf5158cd8624725c9ed4c0999cd43a6d81
naayem <vincentnaayem@gmail.com>
2020-12-02 15:02:09 +0100

    Added sensor and led functionalities, not tested

--- src/telegram_interface/telegram_interface.py
+++ src/telegram_interface/telegram_interface.py
@@ -2,6 +2,8 @@ import telepot
 from telepot.loop import MessageLoop
 from cam_stream import CamStream
 import cv2
+from leds import Dht
+from sensors import LedRing
 
 
 class TelegramInterface:
@@ -12,6 +14,9 @@ class TelegramInterface:
         self.message_loop = MessageLoop(self.bot, self.message_callback).run_as_thread()
 
         self.cam = CamStream(0).start()
+
+        self.led_ring = LedRing
+        self.dht = Dht
 
     def get_bot_info(self):
         return self.bot.getMe()
@@ -33,3 +38,12 @@ class TelegramInterface:
     def start_tutorial(self, chat_id):
         tutorial = "Welcome to lakeye, try to take a picture with /capture_image :)"
         self.bot.sendMessage(chat_id, tutorial)
+
+    def led_on(self, chat_id):
+        self.led_ring.ring_white()
+
+    def led_off(self, chat_id):
+        self.led_ring.led_off()
+
+    def read_sensor(self, chat_id):
+        self.dht.sensor_ht()
